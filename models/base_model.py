9da1c27549c2c09761ec153aa068a3f0354d4e2f
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-10-14 18:20:26 +0300

    import storage

--- models/base_model.py
+++ models/base_model.py
@@ -1,41 +1,38 @@
 #!/usr/bin/python3
 """Define the basemodle class for the project."""
-
+import models
 from uuid import uuid4
 from datetime import datetime
-from models import storage
 
 class BaseModel:
     """Define the BaseModel of the project"""
 
     def __init__(self, *args, **kwargs):
-        """Initialization of a Base instance.
-
-        Args:
-            - *args: list of arguments
-            - **kwargs: dict of key-values arguments
+        """Initializes class instances, attributes(uuid, created/updated)
+        If kwargs is not empty its creates an instance
         """
-
-        if kwargs is not None and kwargs != {}:
-            for key in kwargs:
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'created_at':
+                    created = datetime.fromisoformat(value)
+                    self.created_at = created
+                elif key == 'updated_at':
+                    updated = datetime.fromisoformat(value)
+                    self.updated_at = updated
                 else:
-                    self.__dict__[key] = kwargs[key]
+                    setattr(self, key, value)
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def save(self):
         """update the (updated_at) to current datetime"""
         self.updated_at = datetime.today()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """Return the dictionary of instance of the BaseModel"""
